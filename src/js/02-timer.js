4837542abff5a1bdbd31cdcb5235a28e9c06225a
AnastasiiaSkorik <skorik.anastasiia.97@gmail.com>
2023-01-28 21:57:23 +0200

    add js

---
+++ src/js/02-timer.js
@@ -0,0 +1,53 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.6.min.css";
+
+const refs = {
+    inputEl: document.querySelector('input'),
+    startBtn: document.querySelector('[data-start]'),
+    timerEl: document.querySelector('.timer'),
+    fieldEl: document.querySelector('.field'),
+    daysEl: document.querySelector('[data-days]'),
+    hoursEl: document.querySelector('[data-hours]'),
+    minutesEl: document.querySelector('[data-minutes]'),
+    secondsEl: document.querySelector('[data-seconds]'),
+};
+refs.startBtn.setAttribute('disabled', '');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+        selectedDates[0] - Date.now() >= 0
+        ? refs.startBtn.removeAttribute('disabled', '')
+        : Notiflix.Notify.failure('Please choose a date in the future')
+  },
+};
+flatpickr("#datetime-picker", options);
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+
+function onStartBtnClick() { 
+    setInterval(timer, 1000);
+    refs.startBtn.setAttribute('disabled', '');
+};
+
+function timer() {
+    const diff = new Date(refs.inputEl.value) - Date.now();
+    const days = addLeadingZero(Math.floor(diff / (1000 * 60 * 60 * 24)));
+    const hours = addLeadingZero(Math.floor((diff / (1000 * 60 * 60)) % 24));
+    const minutes = addLeadingZero(Math.floor((diff / (1000 * 60)) % 60));
+    const seconds = addLeadingZero(Math.floor((diff / 1000) % 60));
+
+    refs.daysEl.textContent = days;
+    refs.hoursEl.textContent = hours;
+    refs.minutesEl.textContent = minutes;
+    refs.secondsEl.textContent = seconds;
+};
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+};
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.6.min.css";
+
+const refs = {
+  formEl: document.querySelector('.form'),
+};
+
+refs.formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) { 
+  e.preventDefault();
+  const delay = e.target.delay.value;
+  const step = e.target.step.value;
+  const amount = e.target.amount.value;
+
+  for (let i = 0; i < amount; i += 1) {
+    const newDelay = Number(step) * Number(i) + Number(delay);
+    createPromise(i + 1, newDelay)}};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+    
+    return new Promise(() => {
+      const shouldResolve = Math.random() > 0.3;
+      setTimeout(() => {
+        if (shouldResolve) {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        } else {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        }
+      }, delay)
+    })
+};
+
+
+
